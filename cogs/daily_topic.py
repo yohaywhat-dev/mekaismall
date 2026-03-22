9f77bfaf9a0d5ed05d89baff9f6271655400fbe8
yohaywhat-dev <yohaywhat@gmail.com>
2026-03-22 22:35:32 +0800

    Update daily_topic.py

--- cogs/daily_topic.py
+++ cogs/daily_topic.py
@@ -63,14 +63,14 @@ class DailyTopic(commands.Cog):
     @app_commands.command(name="supertoday", description="查看今日速寫主題")
     async def today(self, interaction: discord.Interaction):
             # 先回應 Discord：我收到了，給我一點時間
-            await interaction.response.defer() 
+        await interaction.response.defer() 
             
-            try:
-                topic = get_today_topic()
-                # 使用 followup 發送，這樣就算超過 3 秒也沒關係
-                await interaction.followup.send(f"🎨 今日速寫主題是：**{topic}** 🎨")
-            except Exception as e:
-                await interaction.followup.send(f"發生錯誤：{e}")    
+        try:
+            topic = get_today_topic()
+            # 使用 followup 發送，這樣就算超過 3 秒也沒關係
+            await interaction.followup.send(f"🎨 今日速寫主題是：**{topic}** 🎨")
+        except Exception as e:
+            await interaction.followup.send(f"發生錯誤：{e}")    
 
 
 async def setup(bot: commands.Bot):
