f3d175950c1aa747ebb42133bf3fcb6ad93b4563
yohaywhat-dev <yohaywhat@gmail.com>
2026-03-22 17:50:18 +0800

    Create keep_alive.py

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+#keep_alive.py
+from flask import Flask
+from threading import Thread
+
+app = Flask(name)
+
+@app.route('/')
+def home():
+    return "Bot is awake ✅"
+
+def run():
+    # host="0.0.0.0" 代表接受所有外部請求
+    # port=8080 你部署的平台通常都支援
+    app.run(host="0.0.0.0", port=8080)
+
+def keep_alive():
+    # 用 Thread 讓 Flask 在背景跑，不阻塞主程式
+    server_thread = Thread(target=run)
+    server_thread.start()
