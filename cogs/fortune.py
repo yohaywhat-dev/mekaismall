fcd1cfc414e101fc7c7da9b40efcbd8fd28f7863
yohaywhat-dev <yohaywhat@gmail.com>
2026-03-23 12:16:30 +0800

    Update fortune.py

--- cogs/fortune.py
+++ cogs/fortune.py
@@ -7,7 +7,7 @@ class Fortune(commands.Cog):
     def __init__(self, bot: commands.Bot):
         self.bot = bot
 
-    @app_commands.command(name="draw", description="抽一張今日運勢籤")
+    @app_commands.command(name="draw", description="小基羅來幫你決定")
     async def draw(self, interaction: discord.Interaction):
         # 1. 定義選項
         fortunes = ["超級好", "好像還不錯喔", "還好", "有點不太好", "別想了"]
